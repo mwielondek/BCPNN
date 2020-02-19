eb03bbea233f38948d15b737d5ead1b5735c140c
mwielondek <miloszw@kth.se>
2020-02-19 15:48:10 +0000

    Clean up

--- singlelayer.py
+++ singlelayer.py
@@ -5,24 +5,6 @@ from sklearn.utils.multiclass import unique_labels
 
 from functools import partial
 
-### TODO
-#
-
-### Necessary
-# predict takes in X = (n_samples, n_features)
-# benchmark against zoo
-
-### Improvements
-# change to class_count_ and feature_count_ like sklearn BernoulliNB
-# caching counts
-# hardcoded n and m (for non binary look into LabelBinarizer sklearn)
-# gain clarity and refactor get_counts as to make j-notation less confusing
-#
-# add predict_log_proba and predict_proba
-# add score function
-#
-# change into matrix mult instead of for loops
-
 class BCPNN(BaseEstimator, ClassifierMixin):
 
     def __init__(self):
@@ -48,18 +30,18 @@ class BCPNN(BaseEstimator, ClassifierMixin):
         X = check_array(X)
         n_samples = X.shape[0]
 
-        # pi = the conditional probability of the outcome yjj' of the variable Yj given observations in A.
+        # pi = the conditional probability of the outcome yjj' of the variable
+        # Yj given observations in A.
         # calc support value s_j = beta_j + Sum_i(w_ji * o_i)
 
-        # TODO check if works without explicit o_i "filtering factor" - we do need it!
         s_values = np.empty((n_samples, self.n_classes_))
-        # TODO can change this into matrix calculation or 2d map? Both for loops.
         for i, x in enumerate(X):
             for j, cls in enumerate(self.classes_):
                 beta = self._get_beta(cls)
 
                 observed_features = np.flatnonzero(x)
-                sigma = sum(map(partial(self._get_weights, j = cls), observed_features))
+                w_j = partial(self._get_weights, j = cls)
+                sigma = sum(map(w_j, observed_features))
 
                 s_values[i][j] = beta + sigma
 
@@ -70,16 +52,14 @@ class BCPNN(BaseEstimator, ClassifierMixin):
         """beta_i = log  (c_i + alpha / n_i) / (C + alpha) where
         C = count of patterns
         c_i = count of train patterns belonging to class i
-        n_i = number of outcomes of Y_i (2 - yes/no?)
+        n_i = number of outcomes of Y_i
         """
-
-        # TODO check n_i correct definition
 
         assert np.issubdtype(type(i), np.integer)
 
         c = self.n_samples_
         ci = self._get_counts(j = i)
-        ni = 2 # TODO hardcoded
+        ni = 2
         a = self.alpha
 
         b = (ci + a / ni) / (c + a)
@@ -87,7 +67,8 @@ class BCPNN(BaseEstimator, ClassifierMixin):
         return np.log(b)
 
     def _get_counts(self, i = None, j = None):
-        """Number of times that X_i (and X_j if j is set) appear (jointly) in the training set.
+        """Number of times that X_i (and X_j if j is set) appear
+        (jointly) in the training set.
 
         i denotes X_i
         j denotes Y_i
@@ -103,8 +84,8 @@ class BCPNN(BaseEstimator, ClassifierMixin):
         assert i is not None or j is not None
 
         # to help with caching always order the args
-#         if j and i > j:
-#             return self._get_counts(j, i)
+        # if j and i > j:
+        #     return self._get_counts(j, i)
 
         counter = 0
 
@@ -128,10 +109,12 @@ class BCPNN(BaseEstimator, ClassifierMixin):
         j implies Y_i, ie class variable
         """
 
-        c = self.n_samples_
-        ci, cj, cij = [self._get_counts(i), self._get_counts(j = j), self._get_counts(i, j)]
-        ni, mj = [2, 2] # number of attributes, TODO change from hardcoded
-        a = self.alpha
+        c   = self.n_samples_
+        ci  = self._get_counts(i)
+        cj  = self._get_counts(j = j)
+        cij = self._get_counts(i, j)
+        ni  = mj = 2 # number of attributes, TODO change from hardcoded
+        a   = self.alpha
 
         w = (cij + a / (ni * mj)) * (c + a) / ((ci + a / ni) * (cj + a / mj))
 
