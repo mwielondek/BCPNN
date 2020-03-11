5cd4e0be73cc4947614f920073a64ce9350eabfe
mwielondek <miloszw@kth.se>
2020-03-11 14:36:03 +0000

    Break out recurrent into singlelayer WIP

--- singlelayer.py
+++ singlelayer.py
@@ -1,119 +1,116 @@
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
-from sklearn.utils.multiclass import unique_labels
 
-from functools import partial, lru_cache
+class BinvecOneHotEncoder:
 
-class BCPNN(BaseEstimator, ClassifierMixin):
+    @staticmethod
+    def transform(X):
+        """
+        Transforms a vector of binary patterns onto the one hot similar form
+        where each individual binary value x_ij represents whether value x_i
+        in the original vector was 1 or 0, where j = {1, 0}.
+        """
+        X_new = np.empty((X.shape[0], X.shape[1]*2))
+        for i, pattern in enumerate(X):
+            for j, attr in enumerate(pattern):
+                jj = j * 2
+                if attr == 1:
+                    X_new[i][jj:jj+2] = [1, 0]
+                else:
+                    X_new[i][jj:jj+2] = [0, 1]
+        return X_new
 
-    def __init__(self, alpha=None):
-        self.alpha = alpha
+    @staticmethod
+    def inverse_transform(X):
+        X_inv = np.empty((X.shape[0], X.shape[1]//2))
+        for i, pattern in enumerate(X):
+            X_inv[i] = pattern[::2]
+        return X_inv
 
-    def fit(self, X, y):
-        X, y = check_X_y(X, y)
-        self.classes_ = unique_labels(y)
-        self.n_classes_ = self.classes_.shape[0]
+
+class BCPNN:
+
+    N_VALUES = 2
+
+    def fit(self, X):
+        self.X_ = X
         self.n_samples_, self.n_features_ = X.shape
 
-        self.X_ = X
-        self.y_ = y
+    def update(self, X):
+        activations = np.empty_like(X)
+        for i, _ in enumerate(activations):
 
-        # Set alpha to some small value, here 1/C is used (Holst 1997)
-        if self.alpha is None:
-            self.alpha = 1 / self.n_samples_
+            beta = self._get_beta(i)
 
-        return self
+            #        sigma = sum_i (log ( sum_i' ( w_ij * P(x_i) ) ))
+            # over hypercolumns ^         ^ over neurons in each hypercolumn
+            sigma = 0
+            for j in range(0, activations.shape[0], self.N_VALUES):
+                inner_sum = 0
+                for k in range(self.N_VALUES):
+                    inner_sum += self._get_weights(j+k, i) * self._get_prob(j)
+                sigma += np.log(inner_sum)
 
-    def predict(self, X):
-        check_is_fitted(self, attributes=None)
+            h = beta + sigma
 
-        X = check_array(X)
-        n_samples = X.shape[0]
+            activations[i] = np.exp(h)
 
-        # pi = the conditional probability of the outcome yjj' of the variable
-        # Yj given observations in A.
-        # calc support value s_j = beta_j + Sum_i(w_ji * o_i)
+        # normalize output in each hypercolumn
+        return self._normalize(activations)
 
-        s_values = np.empty((n_samples, self.n_classes_))
-        for i, x in enumerate(X):
-            for j, cls in enumerate(self.classes_):
-                beta = self._get_beta(cls)
 
-                observed_features = np.flatnonzero(x)
-                w_j = partial(self._get_weights, j = cls)
-                sigma = sum(map(w_j, observed_features))
+    def retrieve(self, X):
+        Y = np.empty_like(X)
+        for i, pattern in enumerate(X):
 
-                s_values[i][j] = beta + sigma
+            y = np.empty_like(pattern)
+            old = pattern
 
-        highest_prob = list(map(np.argmax, s_values))
-        return self.classes_[highest_prob]
+            while not np.allclose(old, y):
+                old = y
+                y = self.update(y)
 
-    @lru_cache(maxsize=None)
+            Y[i] = y
+
+        return Y
+
     def _get_beta(self, i):
-        """beta_i = log  (c_i + alpha / n_i) / (C + alpha) where
-        C = count of patterns
-        c_i = count of train patterns belonging to class i
-        n_i = number of outcomes of Y_i
-        """
+        # log( P( x_i ) )
 
-        assert np.issubdtype(type(i), np.integer)
+        # we deal with log(0) case, as per Holst 1997
+        c = self._get_prob(i)
+        if c == 0:
+            return np.log(1 / (self.n_samples_ ** 2))
+        return np.log(c)
 
-        c = self.n_samples_
-        ci = self._get_counts(j = i)
-        ni = 2
-        a = self.alpha
+    def _get_prob(self, i):
+        # check how many times x_i occured divided by the number of samples
+        return self.X_[:, i].sum() / self.n_samples_
 
-        b = (ci + a / ni) / (c + a)
-
-        return np.log(b)
-
-    @lru_cache(maxsize=None)
-    def _get_counts(self, i = None, j = None):
-        """Number of times that X_i (and X_j if j is set) appear
-        (jointly) in the training set.
-
-        i denotes X_i
-        j denotes Y_i
-
-        Note that if X_i is denoting a class these are equivalent:
-        c_i = count of training inputs belonging to class i
-        c_i = count of times X_i appear in the training set
-
-        c_ij implies P(Y_i, X_j) meaning the count of training samples with
-        feature X_j belonging to class i.
-        """
-
-        assert i is not None or j is not None
-
-        counter = 0
-
-        if i is not None and j is not None:
-            for idx, x in enumerate(self.X_):
-                counter += x[i] and self.y_[idx] == j
-
-        elif j is not None:
-            counter = (self.y_ == j).sum()
-
-        else:
-            for x in self.X_:
-                counter += x[i]
-
-        return counter
+    def _get_joint_prob(self, i, j):
+        # check how many times x_i occured together with x_j
+        # the divided by number of samples
+        return (self.X_[:, i] * self.X_[:, j]).sum() / self.n_samples_
 
     def _get_weights(self, i, j):
-        """Calculate the weights between node i and j = w_ij.
+        # P(x_i, x_j) / ( P(x_i) x P(x_j) )
 
-        j implies Y_i, ie class variable
-        """
+        # we deal with log(0) case, as per Holst 1997
+        ci, cj = self._get_prob(i), self._get_prob(j)
+        cij = self._get_joint_prob(i, j)
+        if ci == 0 or cj == 0:
+            return 0
+        if cij == 0:
+            return np.log(1 / self.n_samples_)
+        return cij / (ci * cj)
 
-        c   = self.n_samples_
-        ci  = self._get_counts(i)
-        cj  = self._get_counts(j = j)
-        cij = self._get_counts(i, j)
-        ni  = mj = 2 # number of attributes, TODO change from hardcoded
-        a   = self.alpha
+    def _normalize(self, activations):
+        # divide by sum_j( e^h_j ) where j are all the possible values of h
 
-        w = (cij + a / (ni * mj)) * (c + a) / ((ci + a / ni) * (cj + a / mj))
+        for i in range(0, activations.shape[0], self.N_VALUES):
 
-        return np.log(w)
+            denominator = sum(map(np.exp, activations[i:i+self.N_VALUES]))
+
+            for k in range(self.N_VALUES):
+                activations[i+k] = activations[i+k] / denominator
+
+        return activations
