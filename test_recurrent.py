f9591f0174cde309bd017db2a9c1c5ca67c29dec
mwielondek <miloszw@kth.se>
2020-03-11 14:08:24 +0000

    Add pytest tests

---
+++ test_recurrent.py
@@ -0,0 +1,41 @@
+from recurrent import BCPNN, BinvecOneHotEncoder as encoder
+import numpy as np
+
+
+class TestBCPNN:
+
+	def setup(self):
+		self.clf = BCPNN()
+
+		self.test_pattern = np.array([
+			[1, 0, 1, 1, 0],
+			[1, 1, 0, 1, 0]
+			])
+		self.transformed_pattern = encoder.transform(self.test_pattern)
+
+		self.clf.fit(self.test_pattern)
+
+
+	def test_encoder_encode(self):
+		transformed = np.array([[1, 0, 0, 1, 1, 0, 1, 0, 0, 1]])
+		assert (self.transformed_pattern[0] == transformed).all()
+
+	def test_encoder_decode(self):
+		inverse = encoder.inverse_transform(self.transformed_pattern)
+		assert (inverse == self.test_pattern).all()
+
+	def test_get_prob(self):
+		f = self.clf._get_prob
+
+		assert f(0) == 1
+		assert f(1) == 0.5
+		assert f(2) == 0.5
+		assert f(3) == 1
+		assert f(4) == 0
+
+	def test_get_joint_prob(self):
+		f = self.clf._get_joint_prob
+
+		assert f(0, 0) == 1
+		assert f(0, 1) == 0.5
+		assert f(1, 4) == 0
