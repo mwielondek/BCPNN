5ce75ef27521aa544a5ad396f931c9b7ca0a9d7c
mwielondek <miloszw@kth.se>
2020-04-09 18:22:56 +0200

    Add max_iter and tol params

--- recurrent.py
+++ recurrent.py
@@ -3,12 +3,15 @@ from feedforward import BCPNN as FF_BCPNN
 
 class BCPNN(FF_BCPNN):
 
-    def __init__(self, predict_mode='rec'):
+    def __init__(self, predict_mode='rec', max_iter=1e3, tol=1e-4):
         # Recursive version shows slight improvement using timeit
         if predict_mode == 'rec':
             self.predict = self._predict_rec
         elif predict_mode == 'iter':
             self.predict = self._predict_iter
+
+        self.MAX_ITER = max_iter
+        self.TOL = tol
 
     def fit(self, X):
         n_samples, n_features = X.shape
@@ -18,13 +21,17 @@ class BCPNN(FF_BCPNN):
     def _predict_iter(self, X):
         input = X
         prev = np.empty_like(X)
-        while not np.allclose(input, prev):
+        iter = 0
+        while iter < self.MAX_ITER and not np.allclose(input, prev, atol=self.TOL):
             prev = input
             input = self.predict_proba(prev)
+            iter += 1
         return input
 
-    def _predict_rec(self, X):
+    def _predict_rec(self, X, iter=0):
+        if iter >= self.MAX_ITER:
+            return X
         next = self.predict_proba(X)
-        if np.allclose(X, next):
+        if np.allclose(X, next, atol=self.TOL):
             return next
-        return self._predict_rec(next)
+        return self._predict_rec(next, iter + 1)
