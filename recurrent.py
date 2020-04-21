0189b789c158bd108e5918d89241a545ca5ef0d4
mwielondek <miloszw@kth.se>
2020-04-21 10:29:18 +0200

    Remove legacy init param

--- recurrent.py
+++ recurrent.py
@@ -3,8 +3,7 @@ from feedforward import BCPNN as FF_BCPNN
 
 class BCPNN(FF_BCPNN):
 
-    def __init__(self, predict_mode='rec', max_iter=1e3, tol=1e-4,
-                    prob_threshold=0.5):
+    def __init__(self, max_iter=1e3, tol=1e-4, prob_threshold=0.5):
         self.MAX_ITER = max_iter
         self.TOL = tol
         self.PROB_THRESHOLD = prob_threshold
