7a4757b7b739b35e2aa9806ff2214cbdb15c5ad7
mwielondek <miloszw@kth.se>
2020-05-14 16:00:00 +0200

    Make encoder work with regular arrays and return int

--- encoder.py
+++ encoder.py
@@ -9,6 +9,7 @@ class BinvecOneHotEncoder:
         where each individual binary value x_ij represents whether value x_i
         in the original vector was 1 or 0, where j = {1, 0}.
         """
+        X = np.array(X)
         X_new = np.empty((X.shape[0], X.shape[1]*2))
         for i, pattern in enumerate(X):
             for j, attr in enumerate(pattern):
@@ -17,7 +18,7 @@ class BinvecOneHotEncoder:
                     X_new[i][jj:jj+2] = [1, 0]
                 else:
                     X_new[i][jj:jj+2] = [0, 1]
-        return X_new
+        return X_new.astype(int)
 
     @staticmethod
     def inverse_transform(X):
