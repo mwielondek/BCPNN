3456850f53f14a6dc365326b6649973e8030fa6a
mwielondek <miloszw@kth.se>
2020-09-22 11:37:10 +0100

    Add transformX capabilities to recurrent modular

--- recurrent_modular.py
+++ recurrent_modular.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .feedforward_modular import BCPNN as ffBCPNN
+from .encoder import OneHotEncoder
 
 class rmBCPNN(ffBCPNN):
     """ A recurrent version of the Bayesian Confidence
@@ -15,14 +16,34 @@ class rmBCPNN(ffBCPNN):
         self.PROB_THRESHOLD = prob_threshold
         self.VERBOSE = verbose
 
-    def fit(self, X, module_sizes=None):
+    def _transformX_enabled(fn):
+        """Adds a transformX parameter and subsequent logic. Function needs to take X as first argument"""
+
+        def wrapper(self, X, *args, transformX=False, **kwargs):
+            if transformX:
+                # if X are not on the complement unit form
+                X = self.encoder.transform(X)
+            return fn(self, X, *args, **kwargs)
+
+        return wrapper
+
+    def fit(self, X, module_sizes=None, transformX=False):
         n_samples, n_features = X.shape
+
+        if transformX:
+            enc = OneHotEncoder()
+            X = enc.fit_transform(X)
+            mod_sz_X = enc.module_sizes_        # only for X
+            module_sizes = np.tile(mod_sz_X, 2) # so we tile them double for Y
+            self.encoder = enc
+
         # correct module_sizes unless set manually; the default implementation sets one big output module
         if module_sizes is None:
+            # n_features//2 for X and again for Y
             module_sizes = np.full(n_features, 2)
-            y_module_count = n_features // 2
         super().fit(X, X, module_sizes=module_sizes)
 
+    @_transformX_enabled
     def predict(self, X, return_binary=False):
         input = X
         prev = np.zeros_like(X)
@@ -38,6 +59,7 @@ class rmBCPNN(ffBCPNN):
             print("BCPNN: reached max iteration limit of ", self.MAX_ITER)
         return input
 
+    @_transformX_enabled
     def score(self, X, y, tol=0.5):
         """Returns the accuracy score for the classifier. Return value cosists
         of two averages; first counts correct number of separate features,
