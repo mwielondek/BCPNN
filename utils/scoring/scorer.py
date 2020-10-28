de3eef0fd36523da050bb872a5e5636c83f06610
mwielondek <miloszw@kth.se>
2020-10-28 15:18:21 +0000

    Amend scorer with patched cls for sharing modsz between clf/enc

--- utils/scoring/scorer.py
+++ utils/scoring/scorer.py
@@ -15,6 +15,29 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 class Scorer:
 
+    class ModuleSizesStore:
+
+        def __init__(self, modsz=None):
+            self.modsz = modsz
+
+    class PatchedOneHotEncoder(OneHotEncoder):
+        store = None
+
+        def fit(self, *args, **kwargs):
+            super().fit(*args, **kwargs)
+            self.store.modsz = self.module_sizes_
+            return self
+
+    class PatchedBCPNN(mBCPNN):
+        store = None
+
+        def __repr__(self):
+            return "Patched mBCPNN()"
+
+        def fit(self, *args, **kwargs):
+            kwargs.update(module_sizes=self.store.modsz)
+            return super().fit(*args, **kwargs)
+
     def __init__(self, clfs_indices=None):
         LIST_CLFS = self._get_clf_list()
         if clfs_indices is not None:
@@ -23,25 +46,13 @@ class Scorer:
         else:
             self.clfs = LIST_CLFS
 
+        self.store = self.ModuleSizesStore()
+        self.PatchedBCPNN.store = self.store
+        self.PatchedOneHotEncoder.store = self.store
+
     def _get_clf_list(self):
-        patched_mbcpnn = mBCPNN()
-
-        def additional_setup(**kwargs):
-            X, y, pipe, cv_score_kwargs = list(map(kwargs.get, ['X', 'y', 'pipe', 'cv_score_kwargs']))
-
-            modsz = None
-            # if OneHotEncoder is one of the steps we need to prerun the pipe due to how
-            # cross_val_score clones the estimators making us lose module_sizes_ attribute
-            if 'onehot_encoder' in pipe.named_steps.keys():
-                pipe[:-1].fit(X,y)
-                modsz = lambda: pipe.named_steps['onehot_encoder'].module_sizes_
-
-            cv_score_kwargs.update(fit_params=dict(clf__module_sizes=modsz))
-
-        patched_mbcpnn.additional_setup = additional_setup
-
         LIST_NB = [MultinomialNB(), BernoulliNB(), GaussianNB()]
-        LIST_BCPNN = [BCPNN(), patched_mbcpnn]
+        LIST_BCPNN = [BCPNN(), self.PatchedBCPNN()]
         LIST_CLFS = LIST_NB + LIST_BCPNN
 
         return LIST_CLFS
@@ -96,7 +107,7 @@ class Scorer:
             estimators.append(('discretizer', KBD(n_bins=5, encode='ordinal', strategy='uniform')))
 
         if 'onehot_encode' in preprocess:
-            estimators.append(('onehot_encoder', OneHotEncoder()))
+            estimators.append(('onehot_encoder', self.PatchedOneHotEncoder()))
 
         if 'complement_encode' in preprocess:
             estimators.append(('complement_encoder', ComplementEncoder()))
@@ -111,15 +122,7 @@ class Scorer:
     def score_pipeline(self, clf, X, y, **kwargs):
         pipeline_kwargs = {k.split('__', 1)[1]: v for k,v in kwargs.items() if k.split('__', 1)[0] == 'pipeline'}
         cv_score_kwargs = {k.split('__', 1)[1]: v for k,v in kwargs.items() if k.split('__', 1)[0] == 'cv_score'}
-
         pipe = self.create_pipeline(clf, **pipeline_kwargs)
-
-        # For special clf that need additional setup or fit params, like mBCPNN
-        try:
-            clf.additional_setup(X=X, y=y, pipe=pipe, cv_score_kwargs=cv_score_kwargs)
-        except AttributeError:
-            pass
-
         return self.cv_score(pipe, X, y, **cv_score_kwargs)
 
     def cv_score(self, clf, X, y, folds=4, seed=0, fit_params={}):
