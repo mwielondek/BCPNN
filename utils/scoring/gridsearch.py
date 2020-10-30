d3e5587516890cb46762cfa4529d532e713ecf3e
mwielondek <miloszw@kth.se>
2020-10-30 16:52:08 +0000

    Add mode key to gridsearch

--- utils/scoring/gridsearch.py
+++ utils/scoring/gridsearch.py
@@ -8,6 +8,7 @@ from ..clusters import get_cluster_ids
 class GridSearch:
 
     decimals_key = '__decimals'
+    mode_key = '__mode'
 
     def fit(self, clf, X, y, params, fit_params={}, verbose=False, scoring_fn=ami, decimals=None):
         clf.fit(X, **fit_params)
@@ -20,7 +21,7 @@ class GridSearch:
             if verbose:
                 print("[{}/{}] Predicting with params {}".format(i+1, len(params), param_set))
             for k,v in param_set.items():
-                if k == self.decimals_key:
+                if k in [self.decimals_key, self.mode_key]:
                     continue
                 setattr(clf, k, v)
             pred = clf.predict(X)
@@ -28,7 +29,11 @@ class GridSearch:
             if self.decimals_key in param_set.keys():
                 decimals = param_set[self.decimals_key]
 
-            clsid = get_cluster_ids(pred, decimals=decimals)
+            mode_param = {}
+            if self.mode_key in param_set.keys():
+                mode_param = dict(mode=param_set[self.mode_key])
+
+            clsid = get_cluster_ids(pred, decimals=decimals, **mode_param)
             score = scoring_fn(clsid, y)
             res['params'].append(param_set)
             res['score'].append(score)
